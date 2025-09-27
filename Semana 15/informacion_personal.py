1b1ef8dde2720a2d6a00f4a235d74758b97af643
julio2004310 <je.quinonezh@uea.edu.ec>
2025-09-27 09:27:43 -0500

    Semana 15

---
+++ Semana 15/informacion_personal.py
@@ -0,0 +1,24 @@
+#informacion_personal
+informacion_personal = {
+    "nombre": "Julio Quiñonez",
+    "ciudad": "Santo Domingo",
+    "profesion": "Tecnologías de la Información",
+    "telefono": "0987810719"
+}
+
+# Imprimir el diccionario completo
+print(informacion_personal)
+
+# Resultado del código:
+# {
+#     "nombre": "Julio Quiñonez",
+#     "ciudad": "Santo Domingo",
+#     "profesion": "Tecnologías de la Información",
+#     "telefono": "0987810719"
+# }
+
+
+
+
+
+
